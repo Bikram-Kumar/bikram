6b7065ec0f176dbba85bc80c9f4484dd6c9c77f1
Bikram Kumar <coolbikram0@gmail.com>
2024-12-23 20:01:13 +0530

    removed padding y from Software Developer hero sec

--- src/components/ui/hero-scenes.jsx
+++ src/components/ui/hero-scenes.jsx
@@ -33,10 +33,10 @@ export function HeroBeams () {
             <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
                 Bikram Kumar,<br/>
                 <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-                    <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                    <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
                         <span className="">Software Developer</span>
                     </div>
-                    <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+                    <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">
                         <span className="">Software Developer</span>
                     </div>
                 </div>
@@ -60,10 +60,10 @@ export function HeroDot() {
             <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
                 Bikram Kumar,<br/>
                 <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
-                    <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-orange-500 via-yellow-500 to-cyan-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                    <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r from-orange-500 via-yellow-500 to-cyan-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
                         <span className="">Software Developer</span>
                     </div>
-                    <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-orange-500 via-yellow-500 to-cyan-500 py-4">
+                    <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-orange-500 via-yellow-500 to-cyan-500">
                         <span className="">Software Developer</span>
                     </div>
                 </div>
