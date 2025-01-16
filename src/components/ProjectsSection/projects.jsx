61ae8f3789916e3ff37b570576444c7a4392b95b
Bikram Kumar <coolbikram0@gmail.com>
2025-01-16 22:46:59 +0530

    fixed typo in python

--- src/components/ProjectsSection/projects.jsx
+++ src/components/ProjectsSection/projects.jsx
@@ -11,7 +11,7 @@ export default function ProjectsSection() {
                     <ProjectBox name="ControlBoard" img="/images/controlboard.jpg" url="https://github.com/Bikram-Kumar/ControlBoard" desc="A PC keyboard for Android with all keys." skills={["java", "gradle", "androidstudio", "bash"]} />
                     <ProjectBox name="Fooduck" img="/images/fooduck.png" url="https://github.com/Bikram-Kumar/fooduck" desc="A cross-platform app to manage and find food recipes." skills={["flutter", "dart"]} />
                     <ProjectBox name="Bad Apple" img="/images/bad-apple.png" url="https://github.com/Bikram-Kumar/BadAppleWithMazes" desc="Render of Bad Apple With Mazes" skills={["html5", "css3", "javascript"]} />
-                    <ProjectBox name="Netflix Bot" img="/icons/python.svg" url="https://github.com/Bikram-Kumar/NetflixBot" desc="A chatbot that tells you about Netflix shows." skills={["Python"]} />
+                    <ProjectBox name="Netflix Bot" img="/icons/python.svg" url="https://github.com/Bikram-Kumar/NetflixBot" desc="A chatbot that tells you about Netflix shows." skills={["python"]} />
                     <ProjectBox name="DStruct" img="/icons/c.svg" url="https://github.com/Bikram-Kumar/DStruct" desc="Data structures library for C" skills={["c", "cmake"]} />
                     <ProjectBox name="GamIsland" img="/images/gamisland.png" url="https://gamisland.onrender.com" desc="A place for the games I create in my free time." skills={["php", "javascript", "postgresql"]} />
                 </div>
