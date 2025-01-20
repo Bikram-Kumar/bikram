39bb147a9ed404919d112159c496399608d3544a
Bikram Kumar <coolbikram0@gmail.com>
2025-01-20 17:06:47 +0530

    fix

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -2,7 +2,7 @@
 export default async function Home() {
   return (
     <div className="h-screen flex items-center justify-center">
-      Projects {visitCounts}
+      Projects
     </div>
   );
 }
