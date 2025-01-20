3827520bf69a4369ea9330d17f5058c1d75f389b
Bikram Kumar <coolbikram0@gmail.com>
2025-01-20 10:57:22 +0530

    visit count

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,7 +1,38 @@
-export default function Home() {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        Projects
-      </div>
+import { MongoClient } from "mongodb";
+import 'dotenv/config'
+
+const client = new MongoClient(process.env.MONGO_URI);
+
+export default async function Home() {
+  const visitCounts = await updateAndGetVisitCounts();
+  return (
+    <div className="h-screen flex items-center justify-center">
+      Projects {visitCounts}
+    </div>
+  );
+}
+
+
+async function updateAndGetVisitCounts () {
+  var visitCounts = 1000;
+  try {
+    await client.connect();
+    const doc = await client.db("Portfolio").collection("visits").findOneAndUpdate(
+        {name: "counts"},
+        {$inc: {"value": 1}},
+        {
+          upsert: true,
+          returnDocument: "after"
+        }
     );
+    visitCounts = doc.value;
+    console.log("Visited");
+  } catch (e) {
+    console.log(e);
+  } finally {
+    await client.close();
   }
+
+  return visitCounts;
+
+}
