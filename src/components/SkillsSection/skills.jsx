7f877e46146649c0b793f7d62c9d379003af1cd3
Bikram Kumar <coolbikram0@gmail.com>
2025-01-25 15:32:40 +0530

    removed gamedev from skills

--- src/components/SkillsSection/skills.jsx
+++ src/components/SkillsSection/skills.jsx
@@ -54,7 +54,7 @@ export default function SkillsSection() {
                     <SkillBox heading={"Mobile Development"} skills={["flutter", "dart", "java", "androidstudio"]} desc="With hands on experience with Android SDK and Flutter, I create powerful and portable applications for mobile devices that satisfy user needs." />
                     <SkillBox heading={"Backend Development"} skills={["nextjs", "express"]} desc="As a backend developer, I create scalable and efficient server-side applications, robust database architectures, and seamless API integrations that power fast, secure, and reliable digital experiences." />
                     <SkillBox heading={"Database Management"} skills={["mongodb", "postgresql", "mysql"]} desc="By harnessing the power of SQL and NoSQL databases, I create flexible and high-performance data management systems that support complex applications and drive business success." />
-                    <SkillBox heading={"Game Development"} skills={["unity", "csharp", "unrealengine", "cpp"]} desc="Outside of work, I enjoy creating video games, combining art, design, and programming to bring my ideas to life." />
+                    {/*<SkillBox heading={"Game Development"} skills={["unity", "csharp", "unrealengine", "cpp"]} desc="Outside of work, I enjoy creating video games, combining art, design, and programming to bring my ideas to life." /> */}
                     <SkillBox heading={"Tools & Technologies"} skills={["git", "bash", "powershell", "docker", "visualstudio", "cmake", "gradle"]} desc="To accelerate development and optimize efficiency, I leverage a range of cutting-edge tools, featuring build automation, containerization, and other expert technologies within my toolkit." />
 
                    
